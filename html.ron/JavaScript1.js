69fe3900122a7a3b8eeeb91c0aacd72d3f0ae641
Ron <ron7prn@gmail.com>
2025-11-11 22:25:17 +0200

    animals2

--- html.ron/JavaScript1.js
+++ html.ron/JavaScript1.js
@@ -1,6 +1,4 @@
-﻿alert("hello world");
-document.write("hello world");
-document.write("<br/>");
-var num = prompt("Enter a number:", 7);
-document.write("You entered: " + num);
-var num = parseInt(prompt("Enter another number:", 10));
+﻿var name = prompt("Enter your name:");
+alert("Hello, " + name + "!");
+var age parseInt(prompt("Enter your age:"));
+alert("You are " + age + " years old.");
