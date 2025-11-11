8bd1222435ee7f6052bc9b8e7c6a981cc89e258a
Ron <ron7prn@gmail.com>
2025-11-11 21:51:34 +0200

    a

---
+++ html.ron/JavaScript1.js
@@ -0,0 +1,6 @@
+﻿alert("hello world");
+document.write("hello world");
+document.write("<br/>");
+var num = prompt("Enter a number:", 7);
+document.write("You entered: " + num);
+var num = parseInt(prompt("Enter another number:", 10));
